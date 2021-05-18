74dcb6e03c9b9f0f3659aa1d8e1849f8ec5f8940
KHALED AHMMED ANIK <59437962+Khaled227@users.noreply.github.com>
2021-05-18 21:39:23 +0600

    Update signUpPage.java

--- Controller/signUpPage.java
+++ Controller/signUpPage.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import static java.lang.Math.min;
+
 public class signUpPage extends AppCompatActivity {
 
     Button save;
@@ -35,12 +37,13 @@ public class signUpPage extends AppCompatActivity {
         }
         String nameS= String.valueOf(name.getText());
         String passwordS= String.valueOf(password.getText());
+        String conPasswordS= String.valueOf(confirmPassword.getText());
         String emailS= String.valueOf(email.getText());
         String roleS= String.valueOf(role.getText());
 
-        if(!checkAllVale())return;//check all values curreck or not;
+        if(!checkAllValue(nameS,passwordS,emailS,roleS,conPasswordS))return;//check all values curreck or not;
 
-        userOfAllType newUser=new userOfAllType(nameS,passwordS,emailS,roleS);
+        userOfAllType newUser=new userOfAllType(nameS,passwordS,emailS,roleS,"no records found");
 
         //save to database;
 
@@ -62,8 +65,74 @@ public class signUpPage extends AppCompatActivity {
         return true;
     }
 
-    public boolean checkAllVale(){
+
+    public static Boolean isNameValid(String val){
+        if(val.isEmpty())return false;
+        return true;
+    }
+
+    public static Boolean isMailValid(String val){
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(val);
+        return m.matches();
+    }
+
+    public static Boolean isPassValid(String val){
+       if(val.length()>=6)return true;
+       return false;
+    }
+
+    public static Boolean isPassMatched(String val,String val1){
+        if(val.length()!=val1.length())return false;
+        for(int i=0;i<Math.min(val.length(),val1.length());i++){
+            if(val.charAt(i)!=val1.charAt(i))return false;
+        }
+        return true;
+    }
+
+    public static Boolean isRoleValid(String val){
+        String temp="user";
+        for(int i=0;i<min(4,val.length());i++){
+            if(temp.charAt(i)!=val.charAt(i))return false;
+            if(i==3)return true;
+        }
+
+        for(int i=0;i<min(4,val.length());i++){
+            if(temp.charAt(i)!=val.charAt(i))return false;
+            if(i==3)return true;
+        }
+        temp="service provider";
+        for(int i=0;i<min(temp.length(),val.length());i++){
+            if(temp.charAt(i)!=val.charAt(i))return false;
+        }
+
+        return true;
+    }
+
+
+
+
+    public static boolean checkAllValue(String name,String pass,String email, String role,String confirmPass){
+        if(!isNameValid(name))return false;
+        if(!isMailValid(email))return false;
+        if(!isPassValid(pass))return false;
+        if(!isPassMatched(pass,confirmPass))return false;
+        if(!isRoleValid(role))return false;
+       // if(!isNameValid(name)||!isMailValid(email)|| isPassValid(pass)|| isRoleValid(role))return false;
         //will complete later
         return true;
     }
+
+    public static Boolean validatepass(String val){
+        if(val.isEmpty()){
+            //  password.setError("Field cannot be empty");
+            return false;
+        }else {
+            //  password.setError(null);
+            //  password.setErrorEnabled(false);
+            return true;
+        }
+    }
+
 }
